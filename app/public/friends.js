fde5768e717c1dda42ee4be78e0205c6f8ab7072
Ksenia D <Ksenia D>
2017-03-08 19:55:53 -0500

    file structure clean up

--- app/public/friends.js
+++ app/public/friends.js
@@ -6,30 +6,26 @@ $(document).ready(function() {
  	var avg = [
  		$('#q1').val(),
  		$('#q2').val(),
- 		$('#q4').val(),
  		$('#q3').val(),
+ 		$('#q4').val(),
  		$('#q5').val(),
  		$('#q6').val(),
- 		$('#q3').val(),
  		$('#q7').val(),
  		$('#q8').val(),
  		$('#q9').val(),
  		$('#q10').val()];
  		
+ 		console.log(avg);
 
 	 newFriend = {
     		name: $('#name').val().trim(),
     		photo: $('#photo').val().trim(),
     	   	avg: avg
-	    },
+	    };
 	  
 	
 	 console.log(newFriend);
-	 $.post("/new_friend", newFriend).done(function(data) {
-
-		});
- }); 
-  
-});
+	 $.post("/new_friend", newFriend).done(function(data) {});
+ });
   
 });
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,5 +1,28 @@
-module.exports = function (api) {
-	app.get("/new_friend", function(req,res){
-		res.send();
-	});
-}
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+var fs = require("fs");
+
+
+module.exports = function (app) {
+	app.post("/new_friend", function(req, res) {
+	  var newFriend = req.body;
+	  //console.log(newTable);
+	  //console.log(JSON.stringify(newTable));
+	  write(newFriend);
+	  // res.redirect("/match");
+
+	 });
+
+  	function write(newFriend) {
+		fs.appendFile("friends_data.txt", JSON.stringify(newFriend), function(err) {
+	    if(err) {
+	        return console.log(err);
+	    }
+
+	    console.log("Friend added!");
+		}); 
+ 	};
+
+};
+
